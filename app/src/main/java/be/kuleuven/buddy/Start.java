256361d27c09d5ef8727bd6d370c7725303814bf
rebecca.tang <rebecca.tang@student.kuleuven.be>
2022-03-15 22:19:26 +0100

    Start interface done, icon changed

--- app/src/main/java/be/kuleuven/buddy/Start.java
+++ app/src/main/java/be/kuleuven/buddy/Start.java
@@ -3,6 +3,7 @@ package be.kuleuven.buddy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class Start extends AppCompatActivity {
 
