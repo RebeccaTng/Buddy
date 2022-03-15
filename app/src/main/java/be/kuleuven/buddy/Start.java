d9d141d83be8ecdce691a20c4b997a4d3496d39a
rebecca.tang <rebecca.tang@student.kuleuven.be>
2022-03-15 16:17:24 +0100

    start activity made

---
+++ app/src/main/java/be/kuleuven/buddy/Start.java
@@ -0,0 +1,14 @@
+package be.kuleuven.buddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
