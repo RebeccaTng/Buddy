835ce32a81ec4df5688ea5a328df46e353d05449
rebecca.tang <rebecca.tang@student.kuleuven.be>
2022-03-17 11:51:22 +0100

    home activity made

---
+++ app/src/main/java/be/kuleuven/buddy/RequestReset.java
@@ -0,0 +1,14 @@
+package be.kuleuven.buddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RequestReset extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_reset);
+    }
+}
