75afa433d82f5f5b9ff7e4eb203659397b8fb71b
ebert.kiekens <ebert.kiekens@student.kuleuven.be>
2022-03-17 12:56:45 +0100

    test

--- app/src/main/java/be/kuleuven/buddy/Login.java
+++ app/src/main/java/be/kuleuven/buddy/Login.java
@@ -5,8 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class Login extends AppCompatActivity {
+    TextView email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +30,25 @@ public class Login extends AppCompatActivity {
     public void goRegister(View caller) {
         Intent goToRegister = new Intent(this, Register.class);
         startActivity(goToRegister);
+    }
+
+    public void goHome(View caller){
+        email = (TextView) findViewById(R.id.emailFill);
+        password = (TextView) findViewById(R.id.passwFill);
+
+        //make the json object
+        JSONObject user = new JSONObject();
+        try {
+            user.put("email", email.getText().toString());
+            user.put("password", password.getText().toString());
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println("test\n");
+        System.out.println(user);
+
+
+
     }
 }
