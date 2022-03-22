3d772e7e0570fda9fbd549f8460e955fd8a90492
ebert.kiekens <ebert.kiekens@student.kuleuven.be>
2022-03-22 16:06:02 +0100

    register

--- app/src/main/java/be/kuleuven/buddy/activities/Register.java
+++ app/src/main/java/be/kuleuven/buddy/activities/Register.java
@@ -4,18 +4,89 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import be.kuleuven.buddy.R;
 
 public class Register extends AppCompatActivity {
     TextView username, email, password, confirmPassword;
+    boolean correctPassword, validEmail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //define the variables
+        username = (TextView) findViewById(R.id.usernameFill);
+        email = (TextView) findViewById(R.id.emailFill);
+        password = (TextView) findViewById(R.id.passwFill);
+        confirmPassword = (TextView) findViewById(R.id.confPasswFill);
+
+        //textwatcher password
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (email.length() == 0){
+                    //display that you need to fill in a password
+                    System.out.println("You need to fill in a password!\n");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String currentPassword = password.getText().toString();
+                //if longer than 8, contains a number and upper case
+                if (currentPassword.length() >= 8 && currentPassword.matches("(.*[0-9].*)") && currentPassword.matches("(.*[A-Z].*)") ){
+                    //display that the password is correct format
+                    System.out.println("Password is the correct format!\n");
+                }
+                else{
+                    System.out.println("Password is not the correct format\n");
+                }
+            }
+        });
+
+        //textwatcher confirmPassword
+        confirmPassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (confirmPassword.getText().toString().equals(password.getText().toString())){
+                    System.out.println("your password is correct\n");
+                }
+                else {
+                    System.out.println("Password is incorrect!\n");
+                }
+            }
+        });
+
+        //textwatcher email. It checks if email is already in database
+        email.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                checkValidEmail();
+            }
+        });
     }
 
     public void goInfo(View caller) {
@@ -38,12 +109,38 @@ public class Register extends AppCompatActivity {
     }
 
     public void checkRegister(View caller){
-        username = (TextView) findViewById(R.id.mqidjf);
-        email = (TextView) findViewById(R.id.emailFill);
-        password = (TextView) findViewById(R.id.passwFill);
-        
+
+    }
 
 
+    public void checkValidEmail(){
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String URL = "https://a21iot03.studev.groept.be/public/checkEmail/";
+        URL = URL + email.getText().toString();
+        System.out.println(URL);
+
+        JsonArrayRequest submitRequest = new JsonArrayRequest(Request.Method.GET, URL, null,
+
+                new Response.Listener<JSONArray>()
+                {
+                    @Override
+                    public void onResponse(JSONArray response)
+                    {
+                        //String valid = response.getJSONObject(0).getBoolean("valid");
+                        System.out.println(response);
+                    }
+                },
+
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        System.out.println("onresponse is fout");
+                    }
+                }
+        );
+        requestQueue.add(submitRequest);
 
     }
 }
