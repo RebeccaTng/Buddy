0611181660f0182d01b951823ac01164290b12cd
rebecca.tang <rebecca.tang@student.kuleuven.be>
2022-05-19 09:31:45 +0200

    Added optino to delete account

--- app/src/main/java/be/kuleuven/buddy/activities/Account.java
+++ app/src/main/java/be/kuleuven/buddy/activities/Account.java
@@ -1,11 +1,27 @@
 package be.kuleuven.buddy.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 import be.kuleuven.buddy.R;
 import be.kuleuven.buddy.account.AccountInfo;
@@ -22,12 +38,21 @@ public class Account extends AppCompatActivity {
 
         username = findViewById(R.id.dyn_username_acc);
         email = findViewById(R.id.dyn_email_acc);
+        AppCompatButton delete = findViewById(R.id.deleteAcc_btn);
 
         if(getIntent().hasExtra("accountInfo")) {
             accountInfo = getIntent().getExtras().getParcelable("accountInfo");
             username.setText(accountInfo.getUsername());
             email.setText(accountInfo.getEmail());
         }
+
+        delete.setOnClickListener(view -> {
+            AlertDialog.Builder deleteBuilder = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.CustomAlert));
+            deleteBuilder.setMessage("Are you sure you want to delete your account?")
+                    .setPositiveButton("Yes", (dialogInterface, i) -> deleteFromDatabase())
+                    .setNegativeButton("No", (dialogInterface, i) -> dialogInterface.cancel())
+                    .show();
+        });
     }
 
     @Override
@@ -54,5 +79,54 @@ public class Account extends AppCompatActivity {
         Intent goToStart = new Intent(this, Start.class);
         startActivity(goToStart);
         this.overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left);
+    }
+
+    private void deleteFromDatabase() {
+        // Connect to database
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = "https://a21iot03.studev.groept.be/public/api/user/deleteAcc";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest (Request.Method.DELETE, url, null,
+                response -> {
+                    //process the response
+                    try {
+                        String Rmessage = response.getString("message");
+                        String Rcomment = response.getString("comment");
+
+                        //check if login is valid
+                        if(Rmessage.equals("AccountDeleted")){
+                            // Toast
+                            Toast toast = Toast.makeText(getApplicationContext(), Rcomment, Toast.LENGTH_LONG);
+                            toast.show();
+                            // Go to back to library
+                            Intent goToStart = new Intent(this, Start.class);
+                            goToStart.putExtra("accountInfo", accountInfo);
+                            startActivity(goToStart);
+                            this.overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left);
+                            finish();
+
+                        } else{
+                            Toast toast = Toast.makeText(getApplicationContext(), R.string.error, Toast.LENGTH_LONG);
+                            toast.show();
+                        }
+                    } catch (JSONException e){ e.printStackTrace(); }},
+
+                error -> {
+                    //process an error
+                    Toast toast = Toast.makeText(getApplicationContext(), R.string.error, Toast.LENGTH_LONG);
+                    toast.show();
+                })
+        {
+            @Override
+            public Map<String, String> getHeaders() {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + accountInfo.getToken());
+                return headers;
+            }
+        };
+        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(
+                0,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        requestQueue.add(jsonObjectRequest);
     }
 }
