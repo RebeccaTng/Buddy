e0fd34b073658d14f4ef8f19560cdaa71f289943
ebert.kiekens <ebert.kiekens@student.kuleuven.be>
2022-05-21 11:50:16 +0200

    email

--- app/src/main/java/be/kuleuven/buddy/activities/RequestReset.java
+++ app/src/main/java/be/kuleuven/buddy/activities/RequestReset.java
@@ -8,18 +8,32 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import be.kuleuven.buddy.R;
+import be.kuleuven.buddy.account.AccountInfo;
 
 public class RequestReset extends AppCompatActivity {
 
     TextView errorMessage;
-
+    TextView email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         setContentView(R.layout.activity_request_reset);
 
+        email = findViewById(R.id.emailFill_request);
         errorMessage = findViewById(R.id.errorMessage_reset);
     }
 
@@ -37,8 +51,65 @@ public class RequestReset extends AppCompatActivity {
 
     //TODO when click on request, send mail
     public void sendmail(View caller) {
-        Toast toast = Toast.makeText(getApplicationContext(), R.string.mailSucces, Toast.LENGTH_LONG);
-        toast.show();
-        goLogin(caller);
+        System.out.println("button clicked");
+        //process the error
+        errorMessage.setVisibility(View.INVISIBLE);
+        email.setBackgroundResource(R.drawable.bg_fill);
+
+        // Make the json object for the body of the post request
+        JSONObject emailBody = new JSONObject();
+        try {
+            emailBody.put("email", email.getText().toString());
+        } catch (JSONException e) { e.printStackTrace(); }
+
+        System.out.println("body made for email:  " + email.getText().toString());
+
+        // Connect to database
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = "https://a21iot03.studev.groept.be/public/api/forgotPassword";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest (Request.Method.POST, url, null,
+                response -> {
+                    //process the response
+                    try {
+                        String Rmessage = response.getString("message");
+                        System.out.println("response: " + Rmessage);
+
+                        if (Rmessage.equals("NoAccount")){
+                            errorMessage.setText(R.string.incorrectEmail);
+                            errorMessage.setVisibility(View.VISIBLE);
+                            email.setBackgroundResource(R.drawable.bg_fill_red);
+                        }
+                        else{
+                            Toast toast = Toast.makeText(getApplicationContext(), R.string.mailSucces, Toast.LENGTH_LONG);
+                            toast.show();
+                            goLogin(caller);
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+
+                error -> {
+                    //process the error
+                    Toast toast = Toast.makeText(getApplicationContext(), R.string.mailSucces, Toast.LENGTH_LONG);
+                    toast.show();
+                    goLogin(caller);
+                })
+        {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public byte[] getBody() {
+                // request body goes here
+                return emailBody.toString().getBytes(StandardCharsets.UTF_8);
+            }
+        };
+
+        requestQueue.add(jsonObjectRequest);
     }
+
 }
