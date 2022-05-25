dedcabf528aedc03c7e78e6edeb215958c971291
rebecca.tang <rebecca.tang@student.kuleuven.be>
2022-05-25 21:58:52 +0200

    deleted unused import

--- app/src/main/java/be/kuleuven/buddy/activities/Home.java
+++ app/src/main/java/be/kuleuven/buddy/activities/Home.java
@@ -16,7 +16,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
